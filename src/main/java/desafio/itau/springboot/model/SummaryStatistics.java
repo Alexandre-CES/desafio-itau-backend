640517bfdec0c69aa61364d4424a340fa5f17e0f
Alexandre <alexandreces666@gmail.com>
2025-03-31 15:47:18 -0300

    small feat: comments

--- src/main/java/desafio/itau/springboot/model/SummaryStatistics.java
+++ src/main/java/desafio/itau/springboot/model/SummaryStatistics.java
@@ -4,14 +4,29 @@ import java.util.ArrayList;
 
 import desafio.itau.springboot.dto.SummaryStatisticsReturn;
 
+/*
+    *calc and store statistics
+ 
+    The constructor receive an ArrayList<Transaction>
+*/
 public class SummaryStatistics {
 
+    /*
+        *initiate every var but min with 0
+
+        ?min can't be initiated with a value cause 0 is the minimum possible-
+        ?and no value can be assigned to be the max
+
+        ?Other values can be initiated with 0-
+        ?because it's the minimum value possible for them
+    */
     private int count = 0;
     private Double sum = 0.0;
     private Double avg = 0.0;
     private Double min;
     private Double max = 0.0;
 
+    //use the ArrayList<Transaction> to calculating stats, storing in variables
     public SummaryStatistics(ArrayList<Transaction> transactions){
         
         if(!transactions.isEmpty()){
@@ -46,11 +61,12 @@ public class SummaryStatistics {
             this.avg = avg / this.count;
             
         }else{
+            //to avoid null results
             this.min = 0.0;
         }
     }
 
-    //returns summary
+    //returns new SummaryStatisticsReturn Object
     public SummaryStatisticsReturn getStatistics(){
         return new SummaryStatisticsReturn(
             this.count,
--- src/main/java/desafio/itau/springboot/service/TransactionService.java
+++ src/main/java/desafio/itau/springboot/service/TransactionService.java
@@ -12,6 +12,7 @@ import desafio.itau.springboot.model.Transaction;
 @Service
 public class TransactionService {
     
+    //List of transactions
     private ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
     //adds a new transaction to the list
@@ -19,25 +20,39 @@ public class TransactionService {
         transactions.add(transaction);
     }
 
+    //Delete all transactions of ArrayList
     public void deleteAllTransactions(){
         transactions.clear();
     }
 
+    //Returns statistics from transactions in the last 60 seconds
     public SummaryStatisticsReturn getStatistics(){
         
+        //New array to store just new transactions
         ArrayList<Transaction> newTransactions = new ArrayList<Transaction>();
 
-        //get transactions in the last 60 seconds
+        //get the last 60 seconds
         OffsetDateTime timeLimit = OffsetDateTime.now().minusSeconds(60);
+
+        //check if list is not empty
         if(!this.transactions.isEmpty()){
+
+            //iterate each transaction
             for(Transaction transaction : this.transactions){
+
+                //check if transaction was created in the timeLimit
                 if(transaction.getDataHora().isAfter(timeLimit)){
+
+                    //add to newTransactions
                     newTransactions.add(transaction);
                 }
             }
         }
         
+        //create a new SummaryStatistic Object with newTransaction list
         SummaryStatistics summaryStatistics = new SummaryStatistics(newTransactions);
+
+        //return statistics
         return summaryStatistics.getStatistics();
     }
 
