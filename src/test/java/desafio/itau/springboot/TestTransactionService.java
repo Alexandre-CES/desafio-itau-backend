35dbb5ae5245d922c3366e563db9703b65a9824a
Alexandre <alexandreces666@gmail.com>
2025-04-02 19:55:43 -0300

    fix and test: decorator and deleteAllTransactions

--- src/test/java/desafio/itau/springboot/TestTransactionService.java
+++ src/test/java/desafio/itau/springboot/TestTransactionService.java
@@ -1,6 +1,5 @@
 package desafio.itau.springboot;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,12 +7,16 @@ import java.time.OffsetDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import desafio.itau.springboot.model.Transaction;
 import desafio.itau.springboot.service.TransactionService;
 
+@SpringBootTest
 public class TestTransactionService {
 
+    @Autowired
     private TransactionService transactionService;
 
     @BeforeEach
@@ -36,5 +39,19 @@ public class TestTransactionService {
         assertFalse(transactionService.getStatistics().getCount() == 0);
     }
 
+    @Test
+    public void deleteAllTransactions(){
+        OffsetDateTime dateTime = OffsetDateTime.now();
+
+        Transaction transaction = new Transaction(123.0, dateTime);
+
+        //check if list isn't empty before test
+        assertTrue(transactionService.getStatistics().getCount() == 0);
+        transactionService.addTransaction(transaction);
+        assertFalse(transactionService.getStatistics().getCount() == 0);
+
+        transactionService.deleteAllTransactions();
+        assertTrue(transactionService.getStatistics().getCount() == 0);
+    }
 
 }
